b1990bb5631ec4b96686943bd4710fffafc050f7
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-09 19:17:21 +0100

    Added unique_string.js

--- unique_string.js
+++ unique_string.js
@@ -4,7 +4,6 @@
 
 function returnUniqueChar(string) {
   // if (typeof string !== 'string') return 'You have to provide a string';
-
   for (let i = 0; i < string.length; i++) {
     let s = string[i];
     if (string.indexOf(s) === string.lastIndexOf(s)) {
@@ -15,16 +14,14 @@ function returnUniqueChar(string) {
 
 returnUniqueChar('racecar');
 
-
 function get1stUnique(string) {
   const strCopy = string.toLowerCase();
   const uniqueChar = [...strCopy].find((char, index, arr) => {
     const arrCopy = [...arr];
     arrCopy.splice(index, 1);
     return !arrCopy.includes(char);
-  })
+  });
   console.log(uniqueChar);
 }
-
 
 // get1stUnique('racecar');
